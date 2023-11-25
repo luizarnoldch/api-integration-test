24bd320f788770b9f401fd0461100b092f690134
luizarnoldch <luizarnoldch@gmail.com>
2023-11-25 16:12:25 -0500

    test fiorela injection

--- lambdas/hello/main.go
+++ lambdas/hello/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	
-	response := events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello World",
+func handler(ctx context.Context, event events.DynamoDBEvent) (error) {
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		log.Fatalf("Error al convertir el objeto event a JSON: %v", err)
+		return err
 	}
-	return response, nil
+	fmt.Println("EVENT---")
+	fmt.Println(event)
+	fmt.Println("----------------------------------------------")
+	fmt.Println("Event JSON:", string(eventJSON))
+	fmt.Println("----------------------------------------------")
+	
+	return nil
 }
 
 func main() {
